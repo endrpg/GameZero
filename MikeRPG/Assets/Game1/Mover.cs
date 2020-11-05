26ca81c32d498805d5e4babfe74f843c4923b019
endrpg <michael420mabes@outlook.com>
2020-11-05 03:48:15 -0500

    Project Start
    The Main Menu, Pause Menu, Audio Mixer Setup, with 3 scenes.
    
    Game 1 is the 2d rpg, Scene 2 is the left to right rpg idea

---
+++ MikeRPG/Assets/Game1/Mover.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Mover : MonoBehaviour
+{
+    private Rigidbody2D HeroRB;
+
+    [SerializeField]
+    private float speed;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        HeroRB = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        HeroRB.velocity = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical")) * speed * Time.deltaTime;
+
+    }
+}
---
+++ MikeRPG/Assets/Game2/Move.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class Move : MonoBehaviour
+{
+    public float speed;
+    public GameObject character;
+
+    //Rigidbody2D rb;
+
+    // Use this for initalization
+    void Start()
+    {
+       // rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            transform.position += Vector3.right * speed * Time.deltaTime;
+        }
+    }
+
+    void FixedUpdate()
+    {
+      
+    }
+
+    public void moveRight()
+    {
+
+        transform.position += Vector3.right * speed * Time.deltaTime;
+        //rb.velocity = new Vector2(speed * move, rb.velocity.y);
+    }
+
+    public void moveLeft()
+    {
+
+        transform.position += Vector3.left * speed * Time.deltaTime;
+        //rb.velocity = new Vector2(speed * move, rb.velocity.y);
+    }
+}
